9145693aae979cba41a9f2505b43d90f331a20de
SemiMute <44712640+SemiMute@users.noreply.github.com>
2022-02-12 15:22:32 -0800

    NekoQOL Name fix

--- config.js
+++ config.js
@@ -1,31 +1,31 @@
 import { @Vigilant, @TextProperty, @ColorProperty, @ButtonProperty, @SwitchProperty, @DecimalSliderProperty, @SliderProperty, @CheckboxProperty, Color, @SelectorProperty } from 'Vigilance';
 
-@Vigilant("Weebchan")
+@Vigilant("NekoQOL")
 class Settings {
     constructor() {
         this.initialize(this);
-        this.setCategoryDescription("Azas Macro Settings", "Weebchan Settings")
+        this.setCategoryDescription("NekoQOL Main", "Main settings for NekoQOL Module")
         this.setCategoryDescription(`Cane Macro`, "Some cane macro stuff idk")
     }
 
     @SwitchProperty({
         name: "S Shaped Macro Auto",
         description: "Is this macro going to activate when you connect to the server",
-        category: "Azas Macro Setting",
+        category: "NekoQOL Main",
     })
     MAIN_S_FARM_AUTO_ON = false;
 
     @SwitchProperty({
         name: "Cane Macro Auto",
         description: "Is this macro going to activate when you connect to the server",
-        category: "Azas Macro Setting",
+        category: "NekoQOL Main",
     })
     MAIN_CANE_FARM_AUTO_ON = false;
 
     @TextProperty({
         name: "Some fucking text",
         description: "some description idk",
-        category: "Azas Macro Setting"
+        category: "NekoQOL Main",
     })
 
     TEXT_INPUT = " "
@@ -33,7 +33,7 @@ class Settings {
     @TextProperty({
         name: "Webhook URL",
         description: "If you dont put a discord one, wyd",
-        category: "Azas Macro Setting",
+        category: "NekoQOL Main",
         subcategory: "Discord Related"
     })
 
@@ -42,7 +42,7 @@ class Settings {
     @TextProperty({
         name: "Discord User ID",
         description: "Set your user ID if you want to get ponged",
-        category: "Azas Macro Setting",
+        category: "NekoQOL Main",
         subcategory: "Discord Related"
     })
 
@@ -51,7 +51,7 @@ class Settings {
     @SwitchProperty({
         name: "Pong the webhook",
         description: "Do you want to get &lPONGED&r in discord",
-        category: "Azas Macro Setting",
+        category: "NekoQOL Main",
         subcategory: "Discord Related"
     })
 
@@ -60,7 +60,7 @@ class Settings {
     @SwitchProperty({
         name: "Use Webhook",
         description: "Do you want this module to send the webhook information such as changing worlds, or reconnecting?",
-        category: "Azas Macro Setting",
+        category: "NekoQOL Main",
         subcategory: "Discord Related"
     })
 
