9389629d9823b8a485cc69c052fc274ca06ea8ed
SemiMute <44712640+SemiMute@users.noreply.github.com>
2022-02-12 20:18:58 -0800

    force look changes // webhook fix

--- features/macros.js
+++ features/macros.js
@@ -1,4 +1,5 @@
 import SettingsNew from "../config";
+import request from "requestV2";
 const CobbleMacroBind = new KeyBind("Toggle Cobble macro", Keyboard.KEY_NONE, "NekoQOL")
 const SMacroBind = new KeyBind("Toggle S Farm macro", Keyboard.KEY_NONE, "NekoQOL")
 const CaneMacrokeyBind = new KeyBind("Toggle Cane macro", Keyboard.KEY_NONE, "NekoQOL")
@@ -22,6 +23,33 @@ let smacro = false;
 let lastTurnAround = new Date();
 let isReconnecting = false
 let randomshit = false
+
+function postWebhook(url, data) {
+    data = getPingWebhook(data)
+    request({
+        url: url,
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+            "User-Agent": "Mozilla/5.0"
+        },
+        body: {
+            username: "NekoQOL Alerts",
+            content: `${data}`,
+            avatar_url: "https://cdn.discordapp.com/attachments/941853201941016666/942204435701047326/loli61_2.png"
+        }
+    });
+}
+
+function getPingWebhook(content) {
+    let msg;
+    if (SettingsNew.MAIN_WEBHOOK_DISCORD_ID !== undefined) {
+        msg = `<@${SettingsNew.MAIN_WEBHOOK_DISCORD_ID}> ${content}`
+    } else {
+        msg = `${content}`
+    }
+    return msg;
+}
 
 register("tick", () => {
     lastY = getBlock
@@ -115,6 +143,8 @@ register("tick", () => {
 
     // S Macro turning around and "failsafe"
     if (smacro == true) {
+        Player.getPlayer().field_70177_z = SettingsNew.S_SHAPED_COORDS_PITCH || 90.0
+        Player.getPlayer().field_70125_A = SettingsNew.S_SHAPED_COORDS_YAW || 0.0
         if (lastY != getBlock) {
             if (new Date().getTime() - lastTurnAround.getTime() < 1000) return;
             lastTurnAround = new Date();
