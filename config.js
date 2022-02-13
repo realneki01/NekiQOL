c76d9b78794a5422bd600ee0279af6dd19f62d5f
SemiMute <44712640+SemiMute@users.noreply.github.com>
2022-02-12 18:06:26 -0800

    Fixed Sorting

--- config.js
+++ config.js
@@ -1,45 +1,38 @@
 import { @Vigilant, @TextProperty, @ColorProperty, @ButtonProperty, @SwitchProperty, @DecimalSliderProperty, @SliderProperty, @CheckboxProperty, Color, @SelectorProperty } from 'Vigilance';
-@Vigilant('Vigilance', 'My Settings Title Example', {
+@Vigilant('NekoQOL', 'NekoQOL Settings Menu', {
     getCategoryComparator: () => (a, b) => {
         // By default, categories, subcategories, and properties are sorted alphabetically.
         // You can override this behavior by returning a negative number if a should be sorted before b,
         // or a positive number if b should be sorted before a.
 
         // In this case, we cam put Not general! to be above general.
-        const categories = ['NekoQOL Main', 'Wart Macro', `Cane Macro`];
+        const categories = ['NekoQOL Main', 'S Shaped Macro', `Cane Macro`];
 
         return categories.indexOf(a.name) - categories.indexOf(b.name);
     }
 })
-@Vigilant("NekoQOL")
 class Settings {
     constructor() {
         this.initialize(this);
         this.setCategoryDescription("NekoQOL Main", "Main settings for NekoQOL Module")
         this.setCategoryDescription(`Cane Macro`, "Some cane macro stuff idk")
+        this.setCategoryDescription(`S Shaped Macro`, `S Shaped Macro Settings`)
+        this.setSubcategoryDescription(`S Shaped Macro`, `S Shaped Coord Settings`, `&7Set the position your player will be looking during the session`)
     }
 
     @SwitchProperty({
         name: "S Shaped Macro Auto",
         description: "Is this macro going to activate when you connect to the server",
-        category: "NekoQOL Main",
+        category: "S Shaped Macro",
     })
     MAIN_S_FARM_AUTO_ON = false;
 
     @SwitchProperty({
         name: "Cane Macro Auto",
         description: "Is this macro going to activate when you connect to the server",
-        category: "NekoQOL Main",
+        category: "Cane Macro",
     })
     MAIN_CANE_FARM_AUTO_ON = false;
-
-    @TextProperty({
-        name: "Some fucking text",
-        description: "some description idk",
-        category: "NekoQOL Main",
-    })
-
-    TEXT_INPUT = ""
 
     @TextProperty({
         name: "Webhook URL",
@@ -82,14 +75,34 @@ class Settings {
         description: "if you dont know what coords are... why",
         category: "Cane Macro",
     })
-    COORDS_Z_1 = ""
+    CANE_COORDS_Z_1 = ""
 
     @TextProperty({
         name: "Z Coords #2",
         description: "if you dont know what coords are... why",
         category: "Cane Macro",
     })
-    COORDS_Z_2 = ""
+    CANE_COORDS_Z_2 = ""
+
+    // S SHAPED MACRO SETTINGS
+    @TextProperty({
+        name: "Pitch",
+        description: "What direction will the macro be facing when farming?",
+        category: "S Shaped Macro",
+        subcategory: "S Shaped Coord Settings",
+        placeholder: "90.0"
+    })
+    S_SHAPED_COORDS_PITCH = ""
+
+    @TextProperty({
+        name: "Yaw",
+        description: "&c&lWARNING: &cSetting it past -90 to 90 will result in an instant watchdog ban!",
+        category: "S Shaped Macro",
+        subcategory: "S Shaped Coord Settings",
+        placeholder: "0.0"
+    })
+    S_SHAPED_COORDS_YAW = ""
+
 
 
 
